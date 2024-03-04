7ebfec37f876493b35fb902f8a4ee297f25b1602
Marc Josef Conanan <coanananmarc@gmail.com>
2024-03-04 11:49:24 +0100

    commit

---
+++ src/main/java/co/develhope/democlean/DemoCleanApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.democlean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/democlean/DemoCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.democlean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
